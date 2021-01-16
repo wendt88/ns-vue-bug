21956fec89c526149d84fd60631c2cfdb6aa2ad7
peter <wendt88@gmail.com>
2021-01-16 12:34:56 +0100

    init

---
+++ app/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <Page>
+        <ActionBar>
+            <Label text="Home"/>
+        </ActionBar>
+
+        <GridLayout>
+            <Button text="Go to Test" @tap="$navigateTo(Test, {
+                props: {
+                    text: 'Hi'
+                }
+            })" />
+        </GridLayout>
+    </Page>
+</template>
+
+<script>
+    import Test from './Test';
+    export default {
+        data () {
+            return {
+                Test
+            };
+        }
+    };
+</script>
---
+++ app/components/Test.vue
@@ -0,0 +1,27 @@
+<template>
+    <Page>
+        <ActionBar>
+            <Label text="Test"/>
+        </ActionBar>
+
+        <ActivityIndicator :busy="!ready" />
+    </Page>
+</template>
+
+<script>
+export default {
+    props: {
+        text: {
+            type: String
+        }
+    },
+    data () {
+        setTimeout(() => {
+            this.$set(this, 'ready', true);
+        }, 3000);
+        return {
+            ready: false
+        };
+    }
+};
+</script>
